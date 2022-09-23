ea175f6570d02768292f90125bef085439331f32
IbrahimAlshubaily <ibra.94@live.com>
2022-09-23 15:31:37 +0300

    refactor binary search to return element index

--- src/main/java/search/BinarySearch.java
+++ src/main/java/search/BinarySearch.java
@@ -1,2 +1,25 @@
-package search;public class BinarySearch {
+package search;
+
+public class BinarySearch {
+
+    public static int find(Integer[] hayStack, int needle) {
+
+        int lo = 0;
+        int hi = hayStack.length - 1;
+        int mid;
+        int needleIdx = -1;
+        while(lo <= hi){
+            mid = lo +  (hi-lo) / 2;
+            if (hayStack[mid] == needle) {
+                needleIdx = mid;
+                break;
+            } else if (hayStack[mid] < needle) {
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
+        return needleIdx;
+    }
+
 }
--- src/test/java/BinarySearchTest.java
+++ src/test/java/BinarySearchTest.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class BinarySearchTest {
+
+import org.junit.jupiter.api.Test;
+import search.BinarySearch;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class BinarySearchTest {
+
+    @Test
+    void testBinarySearch(){
+        Integer[] nums = new Integer[] {0, 2, 4, 6};
+
+        assertEquals(-1, BinarySearch.find(nums, -1));
+
+        assertEquals(0, BinarySearch.find(nums, 0));
+        assertEquals(-1, BinarySearch.find(nums, 1));
+
+        assertEquals(1, BinarySearch.find(nums, 2));
+        assertEquals(-1, BinarySearch.find(nums, 3));
+
+        assertEquals(2, BinarySearch.find(nums, 4));
+        assertEquals(-1, BinarySearch.find(nums, 5));
+
+        assertEquals(3, BinarySearch.find(nums, 6));
+        assertEquals(-1, BinarySearch.find(nums, 7));
+
+    }
 }
